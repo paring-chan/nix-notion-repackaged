768970d5d955dbd2b893ec796fad747fb65524f7
paring <me@paring.moe>
2025-04-21 14:16:05 +0900

    fix: package version

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A very basic flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11"; };
 
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
